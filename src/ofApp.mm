712d375080b60fd37e9a06f687dbb5574c93b6bc
Joshua Noble <joshuajnoble@gmail.com>
2014-09-25 19:04:24 -0500

    first commit

---
+++ src/ofApp.mm
@@ -0,0 +1,120 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+    connected = false;
+    
+    ble = [[BLEDelegate alloc] init];
+    [ble initialize];
+    [ble setApplication:this];
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+void ofApp::onBluetooth()
+{
+    if([ble isLECapableHardware])
+    {
+        [ble startScan];
+    }
+    else
+    {
+        cout << " uh oh, this computer won't work :( :( :( :( " << endl;
+    }
+}
+
+void ofApp::didDiscoverRFduino(CBPeripheral *rfduino)
+{
+    cout << " didDiscoverRFduino " << rfduino.name << endl;
+    
+    if( [[rfduino name] isEqualTo:@"MY RFDUINO NAME"])
+    {
+         [ble connectDevice:rfduino];
+//        ble->connectDevice(rfduino);
+    }
+}
+
+void ofApp::didUpdateDiscoveredRFduino(CBPeripheral *rfduino)
+{
+    cout << " didUpdateDiscoveredRFduino " << endl;
+}
+
+void ofApp::didConnectRFduino(CBPeripheral *rfduino)
+{
+    cout << " didConnectRFduino " << endl;
+    connected = true;
+}
+
+void ofApp::didLoadServiceRFduino(CBPeripheral *rfduino)
+{
+    cout << " didLoadServiceRFduino " << endl;
+}
+
+void ofApp::didDisconnectRFduino(CBPeripheral *rfduino)
+{
+    cout << " didDisconnectRFduino " << endl;
+}
+
+void ofApp::receivedData( unsigned char *data)
+{
+    cout << " got some data! " << endl;
+    
+    unsigned char reply[] = { 'h', 'i' };
+    
+    [ble send:reply len:2];
+    
+}
